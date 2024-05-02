0ad75e9577765e176f1f7efeb5e58376175fd42e
mystick7 <131989400+mystick7@users.noreply.github.com>
2024-05-02 20:55:29 -0300

    Update BOOGA INFINITE STATS V4.lua

--- BOOGA INFINITE STATS V4.lua
+++ BOOGA INFINITE STATS V4.lua
@@ -1,14 +1,14 @@
 Settings = {
     PointSlot = "Slot1"; -- Slot that will receive points
     NamekSlot = "Slot2"; -- Namekian slot that gives points
-    WaitTimeSlotChange = 0.250; -- less = faster (Recommended 0.1 - 0.3) Wait time when talking to slot changer
-    WaitTimeTalk = 0.2; -- (reccommended 0.1 - 0.3 ) Wait time when talking to level NPCs
-    PersonLimit = 1; -- Person limit for auto rejoiner
+    WaitTimeSlotChange = 0.3; -- less = faster (Recommended 0.1 - 0.3) Wait time when talking to slot changer
+    WaitTimeTalk = 0.3; -- (reccommended 0.1 - 0.3 ) Wait time when talking to level NPCs
+    PersonLimit = 50; -- Person limit for auto rejoiner
     ExtraTime = 0; -- If you need more load time increase the value
-    CharacterSlotChangerDelay = 0.320; -- Delay to talk to character slot changer when it finishes talking to kami (0.3 - 0.5 recommended)
-    KamiChatDelay = 0.320; -- Delay talking to kami (0.270 - 0.450 recommended)
+    CharacterSlotChangerDelay = 0.5; -- Delay to talk to character slot changer when it finishes talking to kami (0.3 - 0.5 recommended)
+    KamiChatDelay = 0.450; -- Delay talking to kami (0.270 - 0.450 recommended)
     MaxPoints = math.huge; -- math.huge means infinite
-    AutoStats = false; -- Auto upgrades a stat
+    AutoStats = true; -- Auto upgrades a stat
     Stat = "Phys-Damage"; -- Health-Max,Ki-Max,Phys-Damage,Phys-Resist,Ki-Damage,Ki-Resist,Speed
     IsPointSlotANamekian = false; -- If point slot isnt a namekian it will be faster and more secure, put to true if point slot is a namekian
 }
