063f885bb009f9552a28fc6d7ff3b24ee0eecf7e
mystick7 <131989400+mystick7@users.noreply.github.com>
2024-05-03 16:00:21 -0300

    Update BOOGA INFINITE STATS V4.lua

--- BOOGA INFINITE STATS V4.lua
+++ BOOGA INFINITE STATS V4.lua
@@ -1,11 +1,11 @@
 Settings = {
     PointSlot = "Slot1"; -- Slot that will receive points
     NamekSlot = "Slot2"; -- Namekian slot that gives points
-    WaitTimeSlotChange = 0.3; -- less = faster (Recommended 0.1 - 0.3) Wait time when talking to slot changer
-    WaitTimeTalk = 0.8; -- (reccommended 0.1 - 0.3 ) Wait time when talking to level NPCs
+    WaitTimeSlotChange = 0.1; -- less = faster (Recommended 0.1 - 0.3) Wait time when talking to slot changer
+    WaitTimeTalk = 0.1; -- (reccommended 0.1 - 0.3 ) Wait time when talking to level NPCs
     PersonLimit = 50; -- Person limit for auto rejoiner
-    ExtraTime = 1; -- If you need more load time increase the value
-    CharacterSlotChangerDelay = 0.5; -- Delay to talk to character slot changer when it finishes talking to kami (0.3 - 0.5 recommended)
+    ExtraTime = 0; -- If you need more load time increase the value
+    CharacterSlotChangerDelay = 0.3; -- Delay to talk to character slot changer when it finishes talking to kami (0.3 - 0.5 recommended)
     KamiChatDelay = 0.270; -- Delay talking to kami (0.270 - 0.450 recommended)
     MaxPoints = math.huge; -- math.huge means infinite
     AutoStats = true; -- Auto upgrades a stat
